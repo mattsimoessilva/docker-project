380f80a26681f678980ed8f7cf4afeed5470e5e7
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2024-10-22 17:42:36 -0300

    Test

--- app.test.js
+++ app.test.js
@@ -1,11 +1,25 @@
 const request = require('supertest');
-const { app } = require('./index'); 
+const { app, server } = require('./index'); // Ensure your index.js exports both app and server
 
 describe('GET /', () => {
     it('responds with Hello, World!', (done) => {
         request(app)
             .get('/')
             .expect('Content-Type', /text/)
-            .expect(200, 'Hello, World!', done);
+            .expect(200, 'Hello, World!', (err, res) => {
+                // If there was an error, log it
+                if (err) {
+                    console.error('Test failed:', err);
+                }
+                done(err); // Call done with error if any
+            });
+    });
+
+    afterAll((done) => {
+        if (server) {
+            server.close(done); // Close the server after tests
+        } else {
+            done(); // If no server, just call done
+        }
     });
 });
